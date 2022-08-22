4c1b066231ae29b26f445e48a97a403cf1d7b48a
drbadpig <stuartkirk14@gmail.com>
2022-08-22 21:31:49 +0300

    edit nav component

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,91 +1,196 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+{{--<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">--}}
+{{--    <!-- Primary Navigation Menu -->--}}
+{{--    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">--}}
+{{--        <div class="flex justify-between h-16">--}}
+{{--            <div class="flex">--}}
+{{--                <!-- Logo -->--}}
+{{--                <div class="shrink-0 flex items-center">--}}
+{{--                    <a href="{{ route('dashboard') }}">--}}
+{{--                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />--}}
+{{--                    </a>--}}
+{{--                </div>--}}
+
+{{--                <!-- Navigation Links -->--}}
+{{--                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">--}}
+{{--                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
+{{--                        {{ __('Dashboard') }}--}}
+{{--                    </x-nav-link>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+
+{{--            <!-- Settings Dropdown -->--}}
+{{--            <div class="hidden sm:flex sm:items-center sm:ml-6">--}}
+{{--                <x-dropdown align="right" width="48">--}}
+{{--                    <x-slot name="trigger">--}}
+{{--                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">--}}
+{{--                            <div>{{ Auth::user()->name }}</div>--}}
+
+{{--                            <div class="ml-1">--}}
+{{--                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">--}}
+{{--                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />--}}
+{{--                                </svg>--}}
+{{--                            </div>--}}
+{{--                        </button>--}}
+{{--                    </x-slot>--}}
+
+{{--                    <x-slot name="content">--}}
+{{--                        <!-- Authentication -->--}}
+{{--                        <form method="POST" action="{{ route('logout') }}">--}}
+{{--                            @csrf--}}
+
+{{--                            <x-dropdown-link :href="route('logout')"--}}
+{{--                                    onclick="event.preventDefault();--}}
+{{--                                                this.closest('form').submit();">--}}
+{{--                                {{ __('Log Out') }}--}}
+{{--                            </x-dropdown-link>--}}
+{{--                        </form>--}}
+{{--                    </x-slot>--}}
+{{--                </x-dropdown>--}}
+{{--            </div>--}}
+
+{{--            <!-- Hamburger -->--}}
+{{--            <div class="-mr-2 flex items-center sm:hidden">--}}
+{{--                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">--}}
+{{--                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">--}}
+{{--                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />--}}
+{{--                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />--}}
+{{--                    </svg>--}}
+{{--                </button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+{{--    <!-- Responsive Navigation Menu -->--}}
+{{--    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">--}}
+{{--        <div class="pt-2 pb-3 space-y-1">--}}
+{{--            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">--}}
+{{--                {{ __('Dashboard') }}--}}
+{{--            </x-responsive-nav-link>--}}
+{{--        </div>--}}
+
+{{--        <!-- Responsive Settings Options -->--}}
+{{--        <div class="pt-4 pb-1 border-t border-gray-200">--}}
+{{--            <div class="px-4">--}}
+{{--                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>--}}
+{{--                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>--}}
+{{--            </div>--}}
+
+{{--            <div class="mt-3 space-y-1">--}}
+{{--                <!-- Authentication -->--}}
+{{--                <form method="POST" action="{{ route('logout') }}">--}}
+{{--                    @csrf--}}
+
+{{--                    <x-responsive-nav-link :href="route('logout')"--}}
+{{--                            onclick="event.preventDefault();--}}
+{{--                                        this.closest('form').submit();">--}}
+{{--                        {{ __('Log Out') }}--}}
+{{--                    </x-responsive-nav-link>--}}
+{{--                </form>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
+
+<nav>
+    <div
+        class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 border-b border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95">
+        <div class="max-w-8xl mx-auto">
+            <div class="py-4 lg:px-8 lg:border-0 mx-4 lg:mx-0">
+                <div class="relative flex items-center">
+
+                    <a href="#" class="flex-none overflow-hidden w-auto">
+                        <span class="sr-only">leagueoftalks home page</span>
+                        <x-application-text-logo class="fill-text hover:fill-active"/>
                     </a>
-                </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
+                    <a href="https://www.leagueoflegends.com/" class="ml-6">
+                        <img class="max-h-[25px]" src="lol_logo.png" alt="lol logo">
+                    </a>
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
 
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    <div class="relative hidden lg:flex items-center ml-auto">
+                        <nav class="text-sm leading-6 font-semibold text-slate-700 dark:text-slate-200">
+                            <ul class="flex space-x-8">
+                                <li>
+                                    <a class="hover:text-active" href="#">Категории</a>
+                                </li>
+                                <li>
+                                    <a class="hover:text-active" href="#">Обсуждения</a>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <div class="flex items-center border-l border-slate-200 ml-6 pl-6 dark:border-slate-800">
+                            <label class="sr-only" id="headlessui-listbox-label-3">Тема</label>
+                            <button type="button" id="headlessui-listbox-button-4" aria-haspopup="true"
+                                    aria-expanded="false"
+                                    aria-labelledby="headlessui-listbox-label-3 headlessui-listbox-button-undefined">
+                            <span class="dark:hidden">
+                                <svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round" class="w-6 h-6">
+                                    <path d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                          class="stroke-slate-400 dark:stroke-slate-500"></path>
+                                    <path
+                                        d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
+                                        class="stroke-slate-400 dark:stroke-slate-500"></path>
                                 </svg>
-                            </div>
+                            </span>
+                                <span class="hidden dark:inline"><svg viewBox="0 0 24 24" fill="none" class="w-6 h-6">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                          d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+                                          class="fill-transparent"></path>
+                                    <path
+                                        d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+                                        class="fill-slate-400 dark:fill-slate-500"></path>
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                          d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+                                          class="fill-slate-400 dark:fill-slate-500"></path>
+                                </svg>
+                            </span>
+                            </button>
+
+
+                            <a href="{{ route('user', ['id' => auth()->id()]) }}"
+                               class="ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+                                <span class="sr-only">Profile</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                     viewBox="0 0 24 24"
+                                     stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                </svg>
+                            </a>
+
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); this.closest('form').submit();"
+                                   class="ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+                                    <span class="sr-only">Выйти</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                         viewBox="0 0 24 24"
+                                         stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                                    </svg>
+                                </a>
+                            </form>
+
+                        </div>
+                    </div>
+                    <div class="ml-auto -my-1 lg:hidden">
+                        <button type="button"
+                                class="text-slate-500 w-8 h-8 flex items-center justify-center hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300">
+                            <span class="sr-only">Navigation</span>
+                            <svg width="24" height="24" fill="none" aria-hidden="true">
+                                <path
+                                    d="M12 6v.01M12 12v.01M12 18v.01M12 7a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
                         </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
