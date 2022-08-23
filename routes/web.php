adcb069663b9b05222224b17d36177c0e059afef
drbadpig <stuartkirk14@gmail.com>
2022-08-23 18:28:38 +0300

    move UserController.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\TestContorller;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
