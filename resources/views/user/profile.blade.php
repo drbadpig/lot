61bfb712487b8ef0c69101ce5461e63f9a9a9f69
drbadpig <stuartkirk14@gmail.com>
2022-08-23 18:20:44 +0300

    hide non current user's settings buttons

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,19 +1,29 @@
 <x-app-layout>
+    <!-- Title -->
     <h1 class="hidden">{{ $title }}</h1>
+
+    <!-- Background image -->
     <div class="fixed top-0 right-0 left-0 bottom-0 opacity-40 blur-md bg-cover bg-center bg-no-repeat"
          style="background-image: url('../backgrounds/bg-serapgine-ocean.jpg')">
     </div>
-    <button type="button"
-            class="absolute top-20 right-4 z-30 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 p-4 rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95"
-    >
-        <x-icons.adjustments-icon class="h-6 w-6"/>
-    </button>
-    <button type="button"
-            class="absolute top-40 right-4 z-30 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 p-4 rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95"
-    >
-        <x-icons.cog-icon class="h-6 w-6"/>
-    </button>
+
+    <!-- Settings buttons -->
+    @if (Auth::user()->id == $user->id)
+        <button type="button"
+                class="absolute top-20 right-4 z-30 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 p-4 rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95"
+        >
+            <x-icons.adjustments-icon class="h-6 w-6"/>
+        </button>
+
+        <button type="button"
+                class="absolute top-40 right-4 z-30 text-slate-400 hover:text-slate-500 dark:hover:text-slate-300 p-4 rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95"
+        >
+            <x-icons.cog-icon class="h-6 w-6"/>
+        </button>
+    @endif
+
     <div class="container mx-auto p-8 flex flex-row relative z-10">
+
         <div class="basis-1/4">
             <img class="w-full mb-2 rounded-lg" src="avatar.png" alt="PHOTO">
 
@@ -28,8 +38,8 @@
                 <span class="">Комментарии</span>
                 <span class="text-active ml-auto">{{ thousands_format(count($user->comments)) }}</span>
             </a>
-
         </div>
+
         <div class="basis-3/4 ml-12 border-l pl-12 border-slate-900/10 dark:border-slate-50/[0.06]">
             <h1 class="text-3xl mb-6">{{ $user->username }}</h1>
             <div class="w-96 mb-6">
@@ -52,18 +62,11 @@
             <div
                 class="w-full p-3 mt-4 rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95 flex flex-row flex-wrap">
                 @foreach ($user->achievements as $achievement)
-                    <img class="w-28 h-28" src="{{ $achievement->image }}" title="{{ $achievement->name }}" alt="{{ $achievement->name }}">
+                    <img class="w-28 h-28" src="{{ $achievement->image }}" title="{{ $achievement->name }}"
+                         alt="{{ $achievement->name }}">
                 @endforeach
-{{--                <img class="w-28 h-28" src="achievements/3years.webp" title="3 years with us" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/5years.webp" title="5 years with us" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/100likes.webp" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/100dislikes.webp" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/1000likes.webp" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/1000dislikes.webp" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/100dislikes.webp" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/1000likes.webp" alt="a">--}}
-{{--                <img class="w-28 h-28" src="achievements/1000dislikes.webp" alt="a">--}}
             </div>
         </div>
+
     </div>
 </x-app-layout>
