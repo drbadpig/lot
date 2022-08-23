7b6844a9ef160c28d5596281e2400c44ac61d178
drbadpig <stuartkirk14@gmail.com>
2022-08-23 16:37:25 +0300

    add helpers.php

---
+++ app/Http/helpers.php
@@ -0,0 +1,62 @@
+<?php
+
+if (! function_exists('date_format_my')) {
+    /**
+     * MySql date format to Month, YEAR
+     *
+     * @param $date
+     * @return string
+     */
+    function date_format_my($date): string
+    {
+        // months
+        $months = [
+            'Январь',
+            'Февраль',
+            'Март',
+            'Апрель',
+            'Май',
+            'Июнь',
+            'Июль',
+            'Август',
+            'Сентябрь',
+            'Октябрь',
+            'Ноябрь',
+            'Декабрь'
+        ];
+
+        // get month in russian
+        $month = date('n')-1;
+
+        // reformat to Month, Year
+        return $months[$month].date(", Y", strtotime($date));
+    }
+}
+
+if (! function_exists('thousands_format')) {
+    /**
+     * Number formatter (1000 - 1k etc)
+     *
+     * @param $num
+     * @return string
+     */
+    function thousands_format($num): string
+    {
+        if ($num > 1000) {
+
+            $x = round($num);
+            $x_number_format = number_format($x);
+            $x_array = explode(',', $x_number_format);
+            $x_parts = array('k', 'm', 'b', 't');
+            $x_count_parts = count($x_array) - 1;
+            $x_display = $x;
+            $x_display = $x_array[0] . ((int)$x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
+            $x_display .= $x_parts[$x_count_parts - 1];
+
+            return $x_display;
+
+        }
+
+        return $num;
+    }
+}
