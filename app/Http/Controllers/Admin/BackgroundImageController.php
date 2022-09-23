ca97911d6caabfb4798705751fd0fc48f47d9773
drbadpig <stuartkirk14@gmail.com>
2022-09-24 02:33:42 +0300

    add show background page

--- app/Http/Controllers/Admin/BackgroundImageController.php
+++ app/Http/Controllers/Admin/BackgroundImageController.php
@@ -55,11 +55,12 @@ class BackgroundImageController extends Controller
      * Display the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        return view('admin.background.show', [
+            'background' => BackgroundImage::find($id),
+        ]);
     }
 
     /**
---
+++ resources/views/admin/background/show.blade.php
@@ -0,0 +1,44 @@
+<x-admin-layout title="Фон профиля">
+    <a href="{{ route('admin.background.edit', [$background->id]) }}" class="btn btn-primary">Редактировать</a>
+    <img alt="{{ $background->name }}" src="{{ asset($background->path) }}" width="600px" class="mt-3">
+    <div class="row mt-3 text-lg">
+        <div class="col-sm-3">
+            <span>ID</span>
+        </div>
+        <div class="col-sm-9">
+            {{ $background->id }}
+        </div>
+    </div>
+    <div class="row mt-3 text-lg">
+        <div class="col-sm-3">
+            <span>Имя</span>
+        </div>
+        <div class="col-sm-9">
+            {{ $background->name }}
+        </div>
+    </div>
+    <div class="row mt-3 text-lg">
+        <div class="col-sm-3">
+            <span>Путь</span>
+        </div>
+        <div class="col-sm-9">
+            {{ $background->path }}
+        </div>
+    </div>
+    <div class="row mt-3 text-lg">
+        <div class="col-sm-3">
+            <span>Дата создания</span>
+        </div>
+        <div class="col-sm-9">
+            {{ $background->created_at }}
+        </div>
+    </div>
+    @if($background->id == 1)
+        <p class="mt-3 text-lg">Этот фон нельзя удалить</p>
+    @else
+        <form method="post" action="{{ route('admin.background.destroy', [$background->id]) }}">
+            @csrf
+            <button type="submit" class="btn btn-danger mt-3">Удалить</button>
+        </form>
+    @endif
+</x-admin-layout>
