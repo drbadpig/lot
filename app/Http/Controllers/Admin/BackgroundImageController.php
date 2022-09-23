53fe0350e9c13d099ba2672115e6933041c04169
drbadpig <stuartkirk14@gmail.com>
2022-09-23 21:16:20 +0300

    add backgrounds index page

--- app/Http/Controllers/Admin/BackgroundImageController.php
+++ app/Http/Controllers/Admin/BackgroundImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\BackgroundImage;
 use Illuminate\Http\Request;
 
 class BackgroundImageController extends Controller
@@ -14,7 +15,9 @@ class BackgroundImageController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.background.index', [
+            'backgrounds' => BackgroundImage::all(),
+        ]);
     }
 
     /**
---
+++ resources/views/admin/background/index.blade.php
@@ -0,0 +1,45 @@
+<x-admin-layout title="Фоны профиля">
+
+    <div class="col-12">
+
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Таблица background_images</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <a href="{{ route('admin.background.create') }}" class="btn btn-success mb-3">Создать</a>
+                <div class="dataTables_wrapper dt-bootstrap4">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table id="table" class="table table-bordered table-striped dataTable dtr-inline">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Путь</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($backgrounds as $background)
+                                    <tr>
+                                        <td>{{ $background->id }}</td>
+                                        <td><a href="{{ route('admin.background.show', [$background->id]) }}">{{ $background->path }}</a></td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            $('#table').DataTable();
+        });
+    </script>
+</x-admin-layout>
