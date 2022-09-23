4848c5e97814f28e74b40be9be938827eca296f0
drbadpig <stuartkirk14@gmail.com>
2022-09-23 16:54:20 +0300

    add admin preloader

---
+++ resources/views/components/admin-preloader.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'preloader flex-column justify-content-center align-items-center']) }}>
+    <img class="animation__wobble" src="{{ asset('lot logo.png') }}" alt="lot logo" height="60" width="60">
+</div>
