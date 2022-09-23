20ffe9dbc8ccda5c51cfdb4d1e58da3dac19fee4
drbadpig <stuartkirk14@gmail.com>
2022-09-23 16:54:00 +0300

    add admin layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@isset($title){{ $title }} -@endisset leagueoftalks admin</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('css/OverlayScrollbars.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+    <!-- Icon -->
+    <link rel="icon" href="{{ asset('lot logo.png') }}">
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/datatables/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/datatables/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/datatables/buttons.bootstrap4.min.css') }}">
+
+    <!-- Scripts -->
+    <script src="https://kit.fontawesome.com/71c10ffd0e.js" crossorigin="anonymous"></script>
+    <!-- Css files wih Tailwind -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <x-admin-preloader/>
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-dark">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-text" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="{{ route('home') }}" class="nav-link">На главную</a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="{{ route('admin') }}" class="brand-link">
+            <span class="brand-text font-weight-light">Админ панель</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="{{ route('admin.user.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-users"></i>
+                            <p>
+                                Пользователи
+                            </p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{ $title }}</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+
+            {{ $slot }}
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- Bootstrap -->
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset('js/jquery.overlayScrollbars.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('js/adminlte.js') }}"></script>
+
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('js/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('js/datatables/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('js/datatables/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('js/datatables/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('js/datatables/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('js/datatables/jszip.min.js') }}"></script>
+<script src="{{ asset('js/datatables/pdfmake.min.js') }}"></script>
+<script src="{{ asset('js/datatables/vfs_fonts.js') }}"></script>
+<script src="{{ asset('js/datatables/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('js/datatables/buttons.print.min.js') }}"></script>
+<script src="{{ asset('js/datatables/buttons.colVis.min.js') }}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+{{--<script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>--}}
+{{--<script src="plugins/raphael/raphael.min.js"></script>--}}
+{{--<script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>--}}
+{{--<script src="plugins/jquery-mapael/maps/usa_states.min.js"></script>--}}
+<!-- ChartJS -->
+{{--<script src="plugins/chart.js/Chart.min.js"></script>--}}
+</body>
+</html>v
