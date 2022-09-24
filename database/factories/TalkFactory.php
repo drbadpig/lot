e16979b98337e1e6b3486c6ae87ee40f40913654
drbadpig <stuartkirk14@gmail.com>
2022-09-24 20:04:01 +0300

    remove likes from talks factory and edit text to large text

--- database/factories/TalkFactory.php
+++ database/factories/TalkFactory.php
@@ -25,8 +25,6 @@ class TalkFactory extends Factory
             'text' => fake()->text(),
             'user_id' => fake()->numberBetween(User::first()->id, DB::table('users')->orderBy('id', 'desc')->first()->id),
             'category_id' => fake()->numberBetween(Category::first()->id, DB::table('categories')->orderBy('id', 'desc')->first()->id),
-            'likes' => fake()->numberBetween(0, 2000),
-            'dislikes' => fake()->numberBetween(0, 2000),
         ];
     }
 }
