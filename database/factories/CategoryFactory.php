065c66db7a63178f071457d16360e9b8b35262a3
drbadpig <stuartkirk14@gmail.com>
2022-09-24 06:16:10 +0300

    edit category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\CategoryFolder;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -19,6 +21,7 @@ class CategoryFactory extends Factory
         return [
             'name' => fake()->word(),
             'description' => fake()->sentence(),
+            'category_folder_id' => fake()->numberBetween(CategoryFolder::first()->id, DB::table('category_folders')->orderBy('id', 'desc')->first()->id),
             'image' => '<svg><path></path></svg>',
         ];
     }
