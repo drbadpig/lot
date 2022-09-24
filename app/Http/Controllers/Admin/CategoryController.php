0bc313c5783c3472b13cfe3f893757291f46a13b
drbadpig <stuartkirk14@gmail.com>
2022-09-24 07:20:33 +0300

    add index category functionality

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -10,11 +11,12 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('admin.category.index', [
+            'categories' => Category::all(),
+        ]);
     }
 
     /**
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,47 @@
+<x-admin-layout title="Категории">
+
+    <div class="col-12">
+
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Таблица categories</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <a href="{{ route('admin.category.create') }}" class="btn btn-success mb-3">Создать</a>
+                <div class="dataTables_wrapper dt-bootstrap4">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table id="table" class="table table-bordered table-striped dataTable dtr-inline">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Название</th>
+                                    <th>Описание</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($categories as $category)
+                                    <tr>
+                                        <td>{{ $category->id }}</td>
+                                        <td><a href="{{ route('admin.category.show', [$category->id]) }}">{{ $category->name }}</a></td>
+                                        <td>{{ $category->description }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            $('#table').DataTable();
+        });
+    </script>
+</x-admin-layout>
