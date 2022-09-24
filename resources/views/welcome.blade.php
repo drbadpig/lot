db472155d38e97a5a0e78dc659604d7b132d06ab
drbadpig <stuartkirk14@gmail.com>
2022-09-24 08:54:38 +0300

    update category page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,8 @@
                         @foreach($folder->categories as $category)
                             <div
                                 class="flex rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95 p-3 mb-3">
-                                <a href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 mr-3" fill="none"
-                                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"/>
-                                    </svg>
+                                <a href="#" class="h-12 w-12 mr-3">
+                                    {!! $category->image !!}
                                 </a>
                                 <div class="flex-auto">
                                     <a href="#" class="uppercase text-lg hover:text-active">{{ $category->name }}</a>
