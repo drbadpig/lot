b7326dc530536c1b00df703d37ffd4fc2b450bdf
drbadpig <stuartkirk14@gmail.com>
2022-09-25 20:04:39 +0300

    fix jquery link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <!-- Css files wih Tailwind -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
         <script src="{{asset('js/summernote/summernote-ru-RU.js')}}"></script>
