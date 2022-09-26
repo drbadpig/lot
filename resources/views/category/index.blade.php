e61ac00257b6e36b8ac4fe879ca632c2f0e29996
drbadpig <stuartkirk14@gmail.com>
2022-09-26 16:09:07 +0300

    add 0 talks block

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -13,6 +13,12 @@
             <div class="w-8/12">
                 <div class="mb-12">
                     <h2 class="text-3xl uppercase mb-6">{{ $category->name }}</h2>
+                    @if(count($talks) == 0)
+                        <div
+                            class="flex relative justify-between items-center rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95 p-3 mb-3">
+                            <p>Здесь пусто :( но вы можете стать первым!</p>
+                        </div>
+                    @endif
                     @foreach($talks as $talk)
                         <div
                             class="flex relative justify-between items-center rounded-lg backdrop-blur border border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95 p-3 mb-3">
